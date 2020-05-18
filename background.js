aa522f53c16b09d72bff7b06102bc12431dcd433
Tae Hyoung Jo <taehyoung.jo@gmail.com>
2020-05-18 19:18:14 -0400

    persistence

--- background.js
+++ background.js
@@ -3,8 +3,11 @@ chrome.runtime.onInstalled.addListener(function () {
     console.log("Follows is true");
   });
   chrome.browserAction.onClicked.addListener(function (tab) {
+    console.log("test");
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
       let url = tabs[0].url;
+
+      console.log(url);
 
       chrome.storage.sync.get("follows", function (data) {
         let link =
@@ -12,7 +15,8 @@ chrome.runtime.onInstalled.addListener(function () {
           url +
           "&src=recent_search_click" +
           (data.follows ? "&pf=on" : "");
-        chrome.tabs.create({ url: link });
+        let encoded = encodeURI(link);
+        chrome.tabs.create({ url: encoded });
       });
     });
   });
